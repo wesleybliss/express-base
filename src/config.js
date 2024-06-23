2c7b5301f732c19167fb2e40cc4a23ca71dfd664
Wesley Bliss <wesley.bliss@gmail.com>
2024-06-22 23:28:01 -0500

    WIP

--- src/config.js
+++ src/config.js
@@ -22,42 +22,9 @@ const config = {
     security: {
         tokenSecret: env('TOKEN_SECRET', EnvType.String),
     },
-    database: {
-        type: env('DB_TYPE', EnvType.EnumString, true, {
-            strict: true,
-            values: ['sqlite', 'postgres'],
-        }),
-        sqliteFile: env('SQLITE_FILE', EnvType.String, false),
-        host: env('DB_HOST', EnvType.String, false),
-        port: env('DB_PORT', EnvType.Int, false),
-        user: env('DB_USER', EnvType.String, false),
-        password: env('DB_PASSWORD', EnvType.String, false),
-        database: env('DB_DATABASE', EnvType.String, false),
-        timestampFormat: 'YYYY-MM-DDTHH:mm:ssZ',
-    },
     misc: {
         signupInviteRequired: env('SIGNUP_INVITE_REQUIRED', EnvType.Boolean, false),
     },
-}
-
-// Post initialization tasks
-
-// If SQLite selected, a file must be specified
-if (config.database.type === 'sqlite') {
-    
-    if (!config.database.sqliteFile?.length)
-        throw new Error('Database type set to SQLite, but no SQLITE_FILE value given')
-    
-} else {
-    
-    if (!parseInt(config.database.port, 10))
-        throw new Error('Invalid database port')
-    
-    ['host', 'user', 'password', 'database'].forEach(it => {
-        if (!config.database[it]?.length)
-            throw new Error(`Invalid database value for "${it}`)
-    })
-    
 }
 
 export default config
